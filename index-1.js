ab336873be6ffdce075fcc6302b9ee1744fdc01f
WaizalAp <wavipkenchi@gmail.com>
2020-01-15 21:39:50 +0700

    commit kedua

---
+++ index-1.js
@@ -0,0 +1,6 @@
+var r = 4.5 
+
+document.write('Luas lingkaran     : ' + (3.14 *r*r))
+document.write('<br />')
+document.write('Keliling lingkaran : ' + (2 * 3.14 *r))
+document.write('<br />')
---
+++ index-2.js
@@ -0,0 +1,7 @@
+var suhuCelsius = 34
+
+document.write('Suhu Kelvin     : ' + 34 + 273)
+document.write("<br>")
+document.write('Suhu Fahrendeit : '+9/5 * suhuCelsius + 32)
+document.write("<br>")
+document.write('Suhu Reamur     : ' + 4/5 * suhuCelsius)
